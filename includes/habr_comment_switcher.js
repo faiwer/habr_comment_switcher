cc805f31b70fe42645fe3ca63e7356ebabcd24da
Zubashev Stepan <faiwer@gmail.com>
2012-04-26 23:55:10 +0600

    + jQuery, + scrollTo, + new CSS-style

--- includes/habr_comment_switcher.js
+++ includes/habr_comment_switcher.js
@@ -5,158 +5,121 @@
 +function( w )
 {
 	var d = w.document,
-		$ = d.querySelector.bind( d ),
-		$$ = d.querySelectorAll.bind( d ),
-		test_mode = false;
+		test_mode = false,
+		// test_mode = true,
+		$, engine;
 
 	var Engine = function(){ this._init(); }
 	Engine.prototype =
 	{
 		_init: function()
 		{
+			$ = w.jQuery;
 			this._initConst();
-			if( this._prepareSlider() )
+
+			var $slider = $( this.s.slider );
+			if( !$slider.get( 0 ) )
 			{
-				this._cssInject();
-				this._observe();
+				return false;
 			}
+
+			// prepare button
+			$button = $();
+			var list = { _1up: -1, _2down: +1 };
+
+			for( var name in list )
+			{
+				var $item = $( '<div>',
+					{
+						'class': this.c.button_class,
+						unselectable: 'on',
+						'data-direction': list[ name ],
+					} )
+					.text( list[ name ] > 0 ? '▼' : '▲' )
+					.appendTo( $slider );
+
+				$button = $button.add( $item );
+			}
+
+			$button.click( this._slideClick.bind( this ) );
+
+			// prepare button CSS
+			$( '<style>' )
+				.text( this.css.join( '\n' ) )
+				.appendTo( d.head );
 		},
 
 		_initConst: function()
 		{
-			this._extend( this,
+			$.extend( this,
 			{
 				s: { // selectors
 					slider: '#xpanel',
 					info_panel: test_mode ? '.comment_item > .info' : '.comment_item > .info.is_new',
 				},
 				c: { // classes
-					active: '__hcsc_active'
+					active: '__hcsc_active',
+					button_class: '__hcsc_button',
 				},
 				css: [
 					'.__hcsc_button { border-top: 1px solid white; line-height: 22px; height: 22px; ' +
 					'cursor: pointer; }',
 					'.__hcsc_button:hover { color: white; }',
-					'.info.__hcsc_active { outline: 2px solid #222; }',
+					'.comments_list .comment_item .info.__hcsc_active { background-color: #774141 !important; }',
+					'.info.__hcsc_active * { color: white !important; }',
 				],
-				elem:
-				{
-					button: {
-						tagName: 'div',
-						attr: { 'class': '__hcsc_button', 'unselectable': 'on' },
-					},
-					style_inject: {
-						tagName: 'style',
-						attr: { id: '__habr_comment_switcher_css' }
-					}
-				}
 			} );
 		},
 
-		_extend: function( object, extend )
-		{
-			for( var name in extend ) if( extend.hasOwnProperty( name ) )
-			{
-				object[ name ] = extend[ name ];
-			}
-		},
-
-		_cssInject: function()
-		{
-			var style = this._createElem( this.elem.style_inject ),
-				text = '';
-			for( var i = 0, n = this.css.length; i < n; ++ i )
-			{
-				text += this.css[ i ];
-			}
-			style.innerHTML = text;
-			d.head.appendChild( style );
-		},
-
-		_createElem: function( data )
-		{
-			var item = d.createElement( data.tagName );
-
-			if( data.attr )
-			{
-				for( var rule in data.attr )
-				{
-					item.setAttribute( rule, data.attr[ rule ] );
-				}
-			}
-
-			return item;
-		},
-
-		_prepareSlider: function()
-		{
-			var slider = $( this.s.slider );
-			if( !slider )
-			{
-				return false;
-			}
-
-			this.up_button = this._createElem( this.elem.button );
-			this.up_button.innerHTML = '▲';
-			slider.appendChild( this.up_button );
-
-			this.down_button = this._createElem( this.elem.button );
-			this.down_button.innerHTML = '▼';
-			slider.appendChild( this.down_button );
-
-			return true;
-		},
-
-		_observe: function()
-		{
-			this.up_button.addEventListener( 'click', this._slideClick.bind( this, -1 ), false );
-			this.down_button.addEventListener( 'click', this._slideClick.bind( this, +1 ), false );
-		},
 
 		_checkItems: function()
 		{
-			var items = $$( this.s.info_panel );
+			var $items = $( this.s.info_panel );
 
-			if( !this.items || !this.items.length || !items.length || ( this.items[ 0 ] !== items[ 0 ] ) )
+			if( !this.$items || !this.$items.size() || !$items.size()
+				|| ( this.$items.get( 0 ) !== $items.get( 0 ) ) )
 			{
 				this.position = -1;
-				this.items = items;
+				this.$items = $items;
 			}
 
-			return this.items;
+			return !!this.$items.size();
 		},
 
-		_slideClick: function( diff )
+		// slide button click event
+		_slideClick: function( e )
 		{
-			if( this.current )
+			var $button = $( e.currentTarget ),
+				diff = $button.data('direction');
+
+			if( this.$current )
 			{
-				this.current.classList.remove( this.c.active );
+				this.$current.removeClass( this.c.active );
 			}
 
-			if( !this._checkItems().length )
+			if( !this._checkItems() )
 			{
 				return;
 			}
 
+			// check new position
 			this.position += diff;
 			if( this.position < 0 )
 			{
-				this.position = this.items.length - 1;
+				this.position = this.$items.size() - 1;
 			}
-			else if( this.position >= this.items.length )
+			else if( this.position >= this.$items.size() )
 			{
 				this.position = 0;
 			}
 
-			this.current = this.items[ this.position ];
-			this.current.scrollIntoView( true );
-			this.current.classList.add( this.c.active );
+			this.$current = this.$items.eq( this.position ).addClass( this.c.active );
+			$.scrollTo( this.$current, '100' );
 		}
 	}
 
 	if( w.location.href.indexOf( 'habrahabr.ru' ) > 0 )
 	{
-		var engine = false;
 		d.addEventListener( 'DOMContentLoaded', function()
 		{
 			setTimeout( function(){ engine = new Engine(); }, 1500 );
