52696b538c0a3152bb65d8cbde16491e7911d293
Zubashev Stepan <faiwer@gmail.com>
2012-04-27 22:24:50 +0600

    + new comment counter, + refresh icon, + skin for visited comments

--- includes/habr_comment_switcher.js
+++ includes/habr_comment_switcher.js
@@ -22,6 +22,10 @@
 			{
 				return false;
 			}
+			this.$counter = $slider.find( this.s.counter );
+
+			var $refresh = $slider.find( this.s.refresh );
+			$refresh.click( this._refreshClick.bind( this ) );
 
 			// prepare button
 			$button = $();
@@ -55,23 +59,30 @@
 			{
 				s: { // selectors
 					slider: '#xpanel',
-					info_panel: test_mode ? '.comment_item > .info' : '.comment_item > .info.is_new',
+					counter: '.new',
+					refresh: '.refresh',
+					info_panel: test_mode
+						? '.comment_item > .info'
+						: '.comment_item > .info.is_new, .comment_item > .info.__hcsc_visited',
 				},
 				c: { // classes
+					def_new: 'is_new',
 					active: '__hcsc_active',
+					visited: '__hcsc_visited',
 					button_class: '__hcsc_button',
 				},
 				css: [
 					'.__hcsc_button { border-top: 1px solid white; line-height: 22px; height: 22px; ' +
 					'cursor: pointer; }',
 					'.__hcsc_button:hover { color: white; }',
+					'.comments_list .comment_item .info.__hcsc_visited { background-color: #A48282 !important; }',
 					'.comments_list .comment_item .info.__hcsc_active { background-color: #774141 !important; }',
-					'.info.__hcsc_active * { color: white !important; }',
+					'.info.__hcsc_active *, .info.__hcsc_visited * { color: white !important; }',
 				],
 			} );
 		},
 
-
+		// find all visited && new comment's
 		_checkItems: function()
 		{
 			var $items = $( this.s.info_panel );
@@ -83,8 +94,12 @@
 				this.$items = $items;
 			}
 
+			// show undread new comments count
+			this.$counter.text( Math.max( 0, $items.filter( '.' + this.c.def_new ).size() - 1 ) );
+
 			return !!this.$items.size();
 		},
+
 
 		// slide button click event
 		_slideClick: function( e )
@@ -113,8 +128,21 @@
 				this.position = 0;
 			}
 
-			this.$current = this.$items.eq( this.position ).addClass( this.c.active );
+			this.$current = this.$items.eq( this.position )
+				.addClass( this.c.active )
+				.removeClass( this.c.def_new )
+				.addClass( this.c.visited );
 			$.scrollTo( this.$current, '100' );
+		},
+
+		// click on "refresh" icon
+		// extension need clear all cache and remove "trash"
+		_refreshClick: function()
+		{
+			if( this.$items )
+			{
+				this.$items.removeClass( this.c.active + ' ' + this.c.visited );
+			}
 		}
 	}
 
